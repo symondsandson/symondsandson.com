71c39d3d9841e3a66ca482d11aba14e55bd564cd
Josh Symonds <veraticus@gmail.com>
2013-03-14 01:01:57 -0500

    Initial commit

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+$('#navbar').onePageNav({
+  filter: ':not(.external)',
+  begin: function() {
+    //Hack so you can click other menu items after the initial click
+    $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
+  },
+  end: function() {
+    $('#device-dummy').remove();
+  }
+})
+
+$(window).scroll(function() {
+  if ($('#navbar').offset().top != 0) {
+    $('#navbar').addClass('moving')
+  } else {
+    $('#navbar').removeClass('moving')
+  }
+})
+
+$('#tailored').tipsy({gravity: 'sw', fade: true});
+$('#tradition').tipsy({gravity: 's', fade: true});
+$('#technologies').tipsy({gravity: 'se', fade: true});
+
