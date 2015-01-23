a582c753c26630959158f250a65e7f8fb6b376bd
Ashton Kemerling <ashtonkemerling@gmail.com>
2015-01-22 20:54:03 -0700

    Add dependencies.

--- source/js/index.js
+++ source/js/index.js
@@ -1,3 +1,7 @@
+//= require idangerous.swiper.min
+//= require idangerous.swiper.3dflow.min
+
+
 $(function() {
   $('#navbar').onePageNav({
     filter: ':not(.external)',
@@ -15,4 +19,18 @@ $(function() {
   $('#tailored').tipsy({gravity: 'sw', fade: true});
   $('#tradition').tipsy({gravity: 's', fade: true});
   $('#technologies').tipsy({gravity: 'se', fade: true});
+
+  var mySwiper = new Swiper('#rolodex',{
+    slidesPerView:3,
+    loop:true,
+    //Enable 3D Flow
+    tdFlow: {
+      rotate : 10,
+      stretch :-50,
+      depth: 400,
+      modifier : 1,
+      shadows:true
+    }
+  });
+
 });
