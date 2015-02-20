4b7c3b34a7371748dcbea6fa0ab0fa0d974711b3
Dave Liggat <dliggat@gmail.com>
2015-02-20 14:24:49 -0800

    Add middleman-s3_sync gem

--- config.rb
+++ config.rb
@@ -53,6 +53,20 @@ set :js_dir, 'js'
 
 set :images_dir, 'img'
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'ss-rewrite' # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.region                     = 'us-west-2'     # The AWS region for your bucket.
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -67,7 +81,7 @@ configure :build do
   activate :directory_indexes
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
