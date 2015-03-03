ff0bf76dbdd60265ad2c8ddf7904bda4e21b613d
Dave Liggat <dliggat@gmail.com>
2015-03-03 11:56:33 -0800

    Linkable divs to the contact form

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,9 @@
 $(function() {
   $(document).ready(function() {
+    var scrollSpeed = 1000;
     $('#navbar').onePageNav({
       filter: ':not(.external)',
+      scrollSpeed: scrollSpeed,
       begin: function() {
         //Hack so you can click other menu items after the initial click
         $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
@@ -10,6 +12,12 @@ $(function() {
         $('#device-dummy').remove();
       },
       changeHash: false
+    });
+
+    $(".contact_button").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#have_a_project").offset().top
+      }, scrollSpeed);
     });
 
     $("#contact_form").validate();
